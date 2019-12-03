62cd0af35b874f0e0ff6d63014ea3306cfe94d64
icedCoughfee <brentmelton99@yahoo.com>
2019-12-03 00:01:28 -0800

    add EggGroup, Gender, GrowthRate, Nature, PokeathlonStat, Pokemon, PokemonColor, PokemonForm, PokemonHabitat, PokemonShape, PokemonSpecies, Stat, and Type queries

--- src/index.js
+++ src/index.js
@@ -147,6 +147,58 @@ const resolvers = {
     getCharacteristic: (parent, args) => {
       return getByIdOrName("/characteristic", args);
     },
+    getAllEggGroups: () => getAllConnections("/egg-group"),
+    getEggGroup: (parent, args) => {
+      return getByIdOrName("/egg-group", args);
+    },
+    getAllGenders: () => getAllConnections("/gender"),
+    getGender: (parent, args) => {
+      return getByIdOrName("/gender", args);
+    },
+    getAllGrowthRates: () => getAllConnections("/growth-rate"),
+    getGrowthRate: (parent, args) => {
+      return getByIdOrName("/growth-rate", args);
+    },
+    getAllNatures: () => getAllConnections("/nature"),
+    getNature: (parent, args) => {
+      return getByIdOrName("/nature", args);
+    },
+    getAllPokeathlonStats: () => getAllConnections("/pokeathlon-stat"),
+    getPokeathlonStat: (parent, args) => {
+      return getByIdOrName("/pokeathlon-stat", args);
+    },
+    getAllPokemon: () => getAllConnections("/pokemon"),
+    getPokemon: (parent, args) => {
+      return getByIdOrName("/pokemon", args);
+    },
+    getAllPokemonColors: () => getAllConnections("/pokemon-color"),
+    getPokemonColor: (parent, args) => {
+      return getByIdOrName("/pokemon-color", args);
+    },
+    getAllPokemonForms: () => getAllConnections("/pokemon-form"),
+    getPokemonForm: (parent, args) => {
+      return getByIdOrName("/pokemon-form", args);
+    },
+    getAllPokemonHabitats: () => getAllConnections("/pokemon-habitat"),
+    getPokemonHabitat: (parent, args) => {
+      return getByIdOrName("/pokemon-habitat", args);
+    },
+    getAllPokemonShapes: () => getAllConnections("/pokemon-shape"),
+    getPokemonShape: (parent, args) => {
+      return getByIdOrName("/pokemon-shape", args);
+    },
+    getAllPokemonSpecies: () => getAllConnections("/pokemon-species"),
+    getPokemonSpecies: (parent, args) => {
+      return getByIdOrName("/pokemon-species", args);
+    },
+    getAllStats: () => getAllConnections("/stat"),
+    getStat: (parent, args) => {
+      return getByIdOrName("/stat", args);
+    },
+    getAllTypes: () => getAllConnections("/type"),
+    getType: (parent, args) => {
+      return getByIdOrName("/type", args);
+    },
     // LANGUAGES
     getAllLanguages: () => getAllConnections("/language"),
     getLanguage: (parent, args) => {
@@ -253,6 +305,45 @@ const resolvers = {
     node: obj => getNode(obj)
   },
   CharacteristicConnection: {
+    node: obj => getNode(obj)
+  },
+  EggGroupConnection: {
+    node: obj => getNode(obj)
+  },
+  GenderConnection: {
+    node: obj => getNode(obj)
+  },
+  GrowthRateConnection: {
+    node: obj => getNode(obj)
+  },
+  NatureConnection: {
+    node: obj => getNode(obj)
+  },
+  PokeathlonStatConnection: {
+    node: obj => getNode(obj)
+  },
+  PokemonConnection: {
+    node: obj => getNode(obj)
+  },
+  PokemonColorConnection: {
+    node: obj => getNode(obj)
+  },
+  PokemonFormConnection: {
+    node: obj => getNode(obj)
+  },
+  PokemonHabitatConnection: {
+    node: obj => getNode(obj)
+  },
+  PokemonShapeConnection: {
+    node: obj => getNode(obj)
+  },
+  PokemonSpeciesConnection: {
+    node: obj => getNode(obj)
+  },
+  StatConnection: {
+    node: obj => getNode(obj)
+  },
+  TypeConnection: {
     node: obj => getNode(obj)
   },
   LanguageConnection: {
