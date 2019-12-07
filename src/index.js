012f14ece1b8c80afb65d0f169da6f96d229b67e
icedCoughfee <brentmelton99@yahoo.com>
2019-12-07 03:03:05 -0800

    update server options to reroute Playground and GraphQL HTTP endpoint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-const {
-  GraphQLServer
-} = require("graphql-yoga");
+const { GraphQLServer } = require("graphql-yoga");
 const glue = require("schemaglue");
 
-const {
-  schema,
-  resolver
-} = glue('src/graphql');
+const { schema, resolver } = glue("src/graphql");
 
-require("./logging/schemaLogger")(schema)
+require("./logging/schemaLogger")(schema);
 
 const server = new GraphQLServer({
   typeDefs: schema,
   resolvers: resolver
 });
 
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+const options = {
+  port: 4000,
+  playground: "/",
+  endpoint: "/graphql"
+};
+
+server.start(options, ({ port }) =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
